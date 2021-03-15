9efade8c9590e98d695c6dd6f73edc2a4a1c1539
kva97 <metal0l@yandex.ru>
2021-03-15 20:25:28 +0500

    v2

--- example/android/app/src/main/kotlin/io/carrotquest/flutter_carrotquest_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.carrotquest.flutter_carrotquest_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- example/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_carrotquest_example/main.dart';
-
-void main() {
-  testWidgets('Verify Platform version', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-  });
-}
